a9144f7969841d70b6cac7d8c7dd525ddcc1eff6
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-04-19 18:50:10 +0545

    Javascript Events, Event Bubbling, setInterval & setTimeout completed

---
+++ Javascript/11-Javascript Events, Event Bubbling, setInterval & setTimeout/script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("dblclick", () => {
+    alert("I was clicked.")
+})
+
+/* button.addEventListener("contextmenu", () => {
+    alert("Right click please.")
+}) */
