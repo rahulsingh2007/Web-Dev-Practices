df49ecdf5aaf75c53d8192d3d32c2a042d8d1547
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-04-21 08:07:05 +0545

    Asynchonization in Js

--- Javascript/12-Javascript Callback and Promises/script.js
+++ Javascript/12-Javascript Callback and Promises/script.js
@@ -1,2 +1,9 @@
 console.log("Rahul is human.")
 console.log("Rehan is human.")
+setTimeout(() => {
+    console.log("First second")
+}, 1000);
+setTimeout(() => {
+    console.log("Second second")
+}, 2000);
+console.log("Harry is human.")
