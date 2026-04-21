be0e6cb19eecf6f1d1db731b74a4e4578892999b
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-04-21 08:17:38 +0545

    Callback function

--- Javascript/12-Javascript Callback and Promises/script.js
+++ Javascript/12-Javascript Callback and Promises/script.js
@@ -1,4 +1,4 @@
-console.log("Rahul is human.")
+/* console.log("Rahul is human.")
 console.log("Rehan is human.")
 setTimeout(() => {
     console.log("First second")
@@ -6,4 +6,15 @@ setTimeout(() => {
 setTimeout(() => {
     console.log("Second second")
 }, 2000);
-console.log("Harry is human.")
+console.log("Harry is human.") */
+
+const callback = (arg) => {
+    console.log(arg)
+}
+const loadScript = (src, callback) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+    sc.unload = callback("Harry");
+    document.head.append(sc)
+}
+loadScript("https://www.youtube.com/watch?v=9JaDBYPmiJ0&list=PLu0W_9lII9agq5TrH9XLIKQvv0iaF2X3w&index=75", callback)
