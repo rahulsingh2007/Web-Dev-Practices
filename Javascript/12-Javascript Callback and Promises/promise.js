454866ace8c752e4b30ea5e1c0e8c4840d5cf21e
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-04-22 07:52:50 +0545

    Promise Completed

---
+++ Javascript/12-Javascript Callback and Promises/promise.js
@@ -0,0 +1,49 @@
+console.log('This is promises.');
+
+/* let prom1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Yes I am done.');
+        resolve("Rahul")
+    }, 3000);
+})
+prom1.then((a) => {
+    console.log('a');
+
+}) */
+
+let prom1 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes I am done")
+            resolve("Rahul")
+        }, 1000);
+    }
+})
+
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you 2")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes I am done 2")
+            resolve("Rahul 2")
+        }, 1000);
+    }
+})
+
+
+let p3 = Promise.all([prom1, prom2])
+p3.then((a) => {
+    console.log(a)
+}).catch(err => {
+    console.log(err)
+})
+
+
+
